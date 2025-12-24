f9fc631b1421540fa195485d48df47722eb83aca
wattakeshi <takeshi.watanabe@live.com>
2025-12-24 21:16:27 +0900

    add costumer schema

---
+++ src/api/costumer/controllers/costumer.ts
@@ -0,0 +1,7 @@
+/**
+ * costumer controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::costumer.costumer');
---
+++ src/api/costumer/services/costumer.ts
@@ -0,0 +1,7 @@
+/**
+ * costumer service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::costumer.costumer');
